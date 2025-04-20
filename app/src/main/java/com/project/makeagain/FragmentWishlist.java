c98632a18600bd05a2d3a4ac0a7a145251b3a439
Piyush Lasane <piyushlasane@gmail.com>
2025-04-20 18:45:37 +0530

    Designed empty wishlist Page

--- app/src/main/java/com/project/makeagain/FragmentWishlist.java
+++ app/src/main/java/com/project/makeagain/FragmentWishlist.java
@@ -2,17 +2,36 @@ package com.project.makeagain;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FragmentWishlist extends Fragment {
+
+    TextView exploreButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wishlist, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        exploreButton = view.findViewById(R.id.exploreButton);
+
+        exploreButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            navController.navigate(R.id.fragmentHome);
+        });
     }
 }
